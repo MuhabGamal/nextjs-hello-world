8cae0feddf3ab8ec926244db05333732ff2529c7
MuhabGamal <138332691+MuhabGamal@users.noreply.github.com>
2024-04-25 15:16:21 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
-import Link from "next/link";
+import { GetServerSideProps } from 'next';
 
-export default function Home() {
-  return (
-    <div>
-      Hello World.{" "}
-      <Link href="/about">
-        About
-      </Link>
-    </div>
-  );
+
+export default function Index({ ip }: { ip: string }) {
+    console.log(ip);
+
+    return (
+        <div>{ip}</div>
+    )
 }
+
+export const getServerSideProps = (async context => {
+    const forwarded = context.req.headers['x-forwarded-for'];
+    const ip = forwarded
+        ? (forwarded as string).split(/, /)[0]
+        : context.req.socket.remoteAddress;
+
+    return {
+        props: {
+            ip,
+        },
+    };
+}) satisfies GetServerSideProps;
